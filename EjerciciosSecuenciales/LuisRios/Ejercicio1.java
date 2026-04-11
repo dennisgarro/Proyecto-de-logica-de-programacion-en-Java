aa0ee99718faed32fae853a9d714fc068e8e1ead
Dennis Arlex Garro Agudelo <dennis.garro@globalmvm.com>
2026-04-11 09:39:00 -0500

    crear primero proyecto de luis rios

---
+++ EjerciciosSecuenciales/LuisRios/Ejercicio1.java
@@ -0,0 +1,12 @@
+package EjerciciosSecuenciales.LuisRios;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Ingrese el Numero");
+        int numero = sc.nextInt();
+        System.out.println("el numero ingresado es: " +numero);
+    }
+}
